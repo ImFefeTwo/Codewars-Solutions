6e2592ed171b9b5bc842c1b67a6f33d279c4ab9a
Federico Berrueta <fberr.dev@gmail.com>
2026-04-30 09:01:09 -0300

    docs(python): add NOTES section in Cockroach exercise

--- python/katas/kyu8/cockroach.py
+++ python/katas/kyu8/cockroach.py
@@ -8,3 +8,14 @@ def cockroach_speed(s):
     return int(s / 0.036)
 
 print(cockroach_speed(1.08))
+
+"""  
+============================================================================
+ NOTES
+ "0.036" is a magic number. Could be improved by declaring a constant.
+ Example: KM_H_TO_CM_S = 0.036
+ return int(s / KM_H_TO_CM_S)
+ In addition, we can use math.floor for explicit rounding down.
+ return math.floor(s / KM_H_TO_CM_S)
+ ============================================================================ 
+ """
