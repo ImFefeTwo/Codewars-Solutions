c60b9ae7a84e9701d61b82a961ba9cc93d2fc636
Federico Berrueta <fberr.dev@gmail.com>
2026-05-03 22:48:30 -0300

    feat(java): add A Needle In The Haystack exercise

---
+++ java/Codewars-java/src/katas/kyu8/ANeedleInTheHaystack.java
@@ -0,0 +1,27 @@
+package katas.kyu8;
+
+public class ANeedleInTheHaystack {
+    /*
+    Can you find the needle in the haystack?
+    Write a function findNeedle() that takes an array full of junk but containing one "needle"
+    After your function finds the needle it should return a message (as a string) that says:
+    "found the needle at position" plus the index it found the needle, so:
+
+    Example(Input --> Output)
+    ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5"
+    */
+    public static void main(String[] args) {
+        Object[] objetos = {"junk", "more junk", new String("needle"), "gadget" };
+        System.out.println(findNeedle(objetos));
+    }
+
+    public static String findNeedle(Object[] haystack) {
+        int positionOfNeedle = -1;
+        for (int i = 0; i < haystack.length; i++) {
+            if (haystack[i] instanceof String && "needle".equals(haystack[i])) {
+                positionOfNeedle = i;
+            }
+        }
+        return "found the needle at position " + positionOfNeedle;
+    }
+}
