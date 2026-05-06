40db9e103960244e1ccd29d9d266aaa7413d4fe1
Federico Berrueta <fberr.dev@gmail.com>
2026-05-06 14:01:33 -0300

    feat(java): add Squaring An Argument exercise

---
+++ java/Codewars-java/src/katas/kyu8/SquaringAnArgument.java
@@ -0,0 +1,13 @@
+package katas.kyu8;
+
+public class SquaringAnArgument {
+
+    public static void main(String[] args) {
+
+        System.out.println(square(2));
+    }
+
+    public static int square(int n) {
+        return n * n;
+    }
+}
