2551b8b057d5bbf2b53c732396fdd8becd0119ed
Federico Berrueta <fberr.dev@gmail.com>
2026-05-08 22:06:09 -0300

    feat(python): add Get Character From ASCII Value exercise

---
+++ python/katas/kyu8/get_character_from_ascii_value.py
@@ -0,0 +1,24 @@
+""" Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+
+65 --> 'A'
+97 --> 'a'
+48 --> '0
+
+For ASCII table, you can refer to http://www.asciitable.com/ """
+
+def get_char(c):
+    return chr(c) 
+
+print(get_char(65))
+print(get_char(97))
+print(get_char(48))
+
+"""  
+============================================================================
+ NOTES
+ ord() = Ordinal (Unicode position)
+ chr() = Character
+ ============================================================================ 
+ """
