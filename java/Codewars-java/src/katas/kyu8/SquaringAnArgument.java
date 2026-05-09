502804c65114342784b3723060db56a3967b2913
Federico Berrueta <fberr.dev@gmail.com>
2026-05-08 22:06:08 -0300

    docs(java, python): add description in Welcome To The City, SumArrays and Squaring An Argument exercises

--- java/Codewars-java/src/katas/kyu8/SquaringAnArgument.java
+++ java/Codewars-java/src/katas/kyu8/SquaringAnArgument.java
@@ -1,7 +1,7 @@
 package katas.kyu8;
 
 public class SquaringAnArgument {
-
+    // Now you have to write a function that takes an argument and returns the square of it.
     public static void main(String[] args) {
 
         System.out.println(square(2));
--- java/Codewars-java/src/katas/kyu8/WelcomeToTheCity.java
+++ java/Codewars-java/src/katas/kyu8/WelcomeToTheCity.java
@@ -1,7 +1,13 @@
 package katas.kyu8;
 
 public class WelcomeToTheCity {
+    /*
+    Create a method that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.
 
+    Example:
+    ['John', 'Smith'], 'Phoenix', 'Arizona'
+    This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!
+    */
     public static void main(String[] args) {
         WelcomeToTheCity sayHello = new WelcomeToTheCity();
 
