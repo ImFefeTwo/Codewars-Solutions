850467566471d306cae8fc7b722f9dc3c7cbf1fd
Federico Berrueta <fberr.dev@gmail.com>
2026-05-11 22:01:57 -0300

    feat(java): add DNA To RNA Conversion exercise

---
+++ java/Codewars-java/src/katas/kyu8/DNAToRNAConversion.java
@@ -0,0 +1,26 @@
+package katas.kyu8;
+
+public class DNAToRNAConversion {
+    /*Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').Create a function which translates a given DNA string into RNA.
+    For example:
+    "GCAT"  =>  "GCAU"
+    The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.*/
+
+    public static void main(String[] args) {
+        String text = "Tardaste demasiado?";
+        DNAToRNAConversion exercise = new DNAToRNAConversion();
+        System.out.println(exercise.dnaToRna(text));
+    }
+
+    public String dnaToRna(String dna) {
+        char[] chars = dna.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == 'T') {
+                chars[i] = 'U';
+            }
+        }
+        String newText = new String(chars);
+        return newText;
+    }
+}
